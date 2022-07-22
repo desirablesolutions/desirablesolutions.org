8fd21af22a5afdc359828f73803693fd970b8b67
Jeffrey J. Desir <devops@informandinspire.llc>
2022-07-22 17:22:57 -0400

    rw

--- app/client/views/components/Image/Image.jsx
+++ app/client/views/components/Image/Image.jsx
@@ -8,7 +8,9 @@ const Image = ({ width, height, src, alt, className }) => {
     return (
         <LazyLoadImage
             classNmae={className}
+            visibleByDefault={true}
             alt={alt}
+            placeHolder={`data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='${width}' height='${height}' viewBox='0 0 ${width} ${height}'%3E%3C/svg%3E`}
             height={height}
             effect="black-and-white"
             src={src}

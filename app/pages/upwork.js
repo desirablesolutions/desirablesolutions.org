8e11efb45a213ecaa9b35830d5ec793876b48b4b
Jeffrey Desir <8616589+desirtech@users.noreply.github.com>
2022-07-30 16:49:40 -0400

    dwd

---
+++ app/pages/upwork.js
@@ -0,0 +1,14 @@
+import { useEffect } from "react";
+
+export default function UpworkExternal() {
+
+
+    useEffect(() => {
+
+        window.location.href = "https://www.upwork.com/ag/desirablesolutions";
+    })
+
+    return (<>
+    
+    </>)
+}

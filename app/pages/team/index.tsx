b228191c50e315b663049f4d641cd8b851e7528a
Jeffrey J. Desir <devops@informandinspire.llc>
2022-08-27 19:54:29 -0400

    wa

--- app/pages/team/index.tsx
+++ app/pages/team/index.tsx
@@ -1,12 +1,10 @@
 import PageLayout from "@layouts/PageLayout"
-import TeamGallery from "@views/includes/TeamGallery"
 
 
 export default function TeamPage() {
 
     return (
         <PageLayout>
-            <TeamGallery />
 
         </PageLayout>
 

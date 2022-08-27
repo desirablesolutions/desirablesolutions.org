5b61584da7ded63637ed24cb507190a5ee0d7047
Jeffrey J. Desir <devops@informandinspire.llc>
2022-08-27 17:07:30 -0400

    []

--- app/pages/projects/[projectID].tsx
+++ app/pages/projects/[projectID].tsx
@@ -1,59 +1,12 @@
 
 
-import ProjectLayout from "@layouts/ProjectLayout";
-import find from "@utils/find"
-import projects from "@db/projects"
-import useMeta from "@hooks/useMeta"
-import { useEffect } from "react"
 
 export default function ProjectPage({ projectData }) {
 
 
     return (
-        <ProjectLayout
-            title={projectData.name}
-            impressum={projectData.impressum}
-            description={projectData.description}
-            covers={projectData.covers}
-           />
+       <>
+       </>
     )
 }
 
-
-export async function getStaticPaths() {
-
-    const paths = projects.map(project => {
-        const projectId = project.id
-        return {
-            params: {
-                projectId
-            }
-        }
-    })
-
-    console.log(paths)
-
-    return {
-        paths,
-        fallback: false
-    }
-}
-
-
-
-export async function getStaticProps({ params }) {
-    const projectId = params.projectId
-
-    const projectData = find({
-        arr: projects,
-        id: projectId
-    })
-
-    console.log(projectData)
-
-    return {
-        props: {
-            projectData: projectData
-        }
-    }
-}
--- app/pages/projects/index.tsx
+++ app/pages/projects/index.tsx
@@ -1,15 +1,10 @@
 import PageLayout from "@layouts/PageLayout/"
-import ProjectsPortfolio from "@includes/ProjectsPortfolio"
-import ProofOfModels from "@includes/ProofOfModels"
-import ProjectsManifest from "@includes/ProjectsManifest"
 
 export default function ProjectsPage() {
 
     return (
         <PageLayout>
-            <ProjectsPortfolio />
-            <ProjectsManifest />
-            <ProofOfModels />
+      
         </PageLayout>
     )
 }

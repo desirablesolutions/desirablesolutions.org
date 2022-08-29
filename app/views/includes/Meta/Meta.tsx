41747445fed4bd0b1a26adf1b4379d0e223b4537
Jeffrey J. Desir <devops@informandinspire.llc>
2022-08-29 13:29:45 -0400

    ds

--- app/views/includes/Meta/Meta.tsx
+++ app/views/includes/Meta/Meta.tsx
@@ -21,7 +21,7 @@ const Meta = ({ pageTitle }) => {
       <Icons />
 
       <title>{title} | {pageTitle}</title>
-      <link rel="preload" href="/assets/fonts/the-seasons/the-seasons-Regular.ttf" as="font" type="font/ttf" crossOrigin={true} />
+      <link rel="preload" href="/assets/fonts/the-seasons/the-seasons-Regular.ttf" as="font" type="font/ttf" crossOrigin={"true"} />
       <meta name="theme-color" content="#000000" />
       <meta charSet="UTF-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />

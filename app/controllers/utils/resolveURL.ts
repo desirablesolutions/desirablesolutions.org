2163a2887f260e43664f4cf8306dab9753dbfcd0
Jeffrey J. Desir <devops@informandinspire.llc>
2022-08-30 02:26:46 -0400

    funny

--- app/controllers/utils/resolveURL.ts
+++ app/controllers/utils/resolveURL.ts
@@ -1,4 +1,4 @@
-const LOCALHOST = process.env.NODE_ENV == "development" ? "http://localhost:3000" : ""
+const LOCALHOST = process.env.NODE_ENV == "development" ? "http://localhost:3000" : "https://desirable.solutions"
 
 const resolveURL = (url: string) => {
     return `${LOCALHOST}${url}`

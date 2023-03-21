adac08c435f3cc2aa60b0b331dbff9af98205110
Jeffrey J. Desir <devops@informandinspire.llc>
2023-03-21 00:43:16 -0400

    [[...]]]

--- DOCUMENTATION.md
+++ DOCUMENTATION.md
@@ -1,3 +1,5 @@
 (load:docs/TITLE.md)
+(load:docs/LOGO.md)
+
 (load:docs/CHANGELOG.md)
 (load:docs/LICENSE.md)
--- www/tailwind.config.cjs
+++ www/tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}', './public/**/*.{jpg, png, pdf, mp4}'],
 	theme: {
 		extend: {
 			backgroundImage: {
